112058d78a483ec66dc3887e4db3f5cd4f972778
admcloud <46247103+admcloud@users.noreply.github.com>
2019-01-29 18:55:08 +0300

    Update getLeads.m

--- getLeads.m
+++ getLeads.m
@@ -136,7 +136,8 @@ in
             else Table.ExpandTableColumn(expandStatusesName, "GroupName", {"name"}, {"GroupName.name"}),
 
         delFinal = Table.RemoveColumns(expandGroupsName,{"created_user_id", "responsible_user_id", "group_id", "pipeline_id", "status_id", "custom_fields"}),
-        //renameFinal = Table.RenameColumns(delFinal,{{"Tags.1", "Tags"}})
+        delFinal1 = Table.RenameColumns(delFinal,{{"Tags", "Tags.e"}}),
+        renameFinal = Table.RenameColumns(delFinal1,{{"Tags.1", "Tags"}})
 
 in
     renameFinal
